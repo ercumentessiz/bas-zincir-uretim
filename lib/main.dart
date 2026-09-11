62642930e3ea10b545b3c8162431d9f7675d7954
ercumentessiz <pdrercumentessiz@gmail.com>
2026-09-11 14:55:25 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+import 'package:flutter/foundation.dart' show compute;
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
